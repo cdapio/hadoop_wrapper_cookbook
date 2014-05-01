c57903c503f14f16d45ae55fd311a44fff585530
Chris Gianelloni <chris@continuuity.com>
2014-05-01 14:14:59 -0700

    Initial spec subs for other recipes. These are known to fail and that is expected.

---
+++ spec/hbase_master_init_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'hadoop_wrapper::hbase_master_init' do
+  context 'on Centos 6.4 x86_64' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.automatic['memory']['total'] = '4099400kB'
+        stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
+        stub_command('update-alternatives --display hbase-conf | grep best | awk \'{print $5}\' | grep /etc/hbase/conf.chef').and_return(false)
+      end.converge(described_recipe)
+    end
+
+    it 'does nothing yet' do
+      expect(chef_run).to do_nothing
+    end
+  end
+end
---
+++ spec/jce_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'hadoop_wrapper::jce' do
+  context 'on Centos 6.4 x86_64' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.automatic['memory']['total'] = '4099400kB'
+      end.converge(described_recipe)
+    end
+
+    it 'does nothing yet' do
+      expect(chef_run).to do_nothing
+    end
+  end
+end
