3bb3dcb7c3be7bc5d4325b9cec1348f0363c0501
Chris Gianelloni <chris@continuuity.com>
2014-05-01 15:01:17 -0700

    More stubs

--- spec/hbase_master_init_spec.rb
+++ spec/hbase_master_init_spec.rb
@@ -8,6 +8,8 @@ describe 'hadoop_wrapper::hbase_master_init' do
         node.automatic['memory']['total'] = '4099400kB'
         node.default['hbase']['hbase_site']['hbase.rootdir'] = 'hdfs://fauxhai.local/hbase'
         node.default['hbase']['hbase_site']['hbase.zookeeper.quorum'] = 'fauxhai.local'
+        stub_command('hdfs dfs -test -d hdfs://fauxhai.local/hbase').and_return(false)
+        stub_command('hdfs dfs -test -d /tmp/hbase-staging').and_return(false)
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hbase-conf | grep best | awk \'{print $5}\' | grep /etc/hbase/conf.chef').and_return(false)
       end.converge(described_recipe)
