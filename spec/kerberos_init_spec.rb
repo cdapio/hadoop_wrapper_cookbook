4c4bfff6b2674517e794bead6bd161f46ec490fb
Chris Gianelloni <chris@continuuity.com>
2014-05-01 15:47:58 -0700

    Add more stubs and check user creation

--- spec/kerberos_init_spec.rb
+++ spec/kerberos_init_spec.rb
@@ -9,11 +9,15 @@ describe 'hadoop_wrapper::kerberos_init' do
         node.default['hadoop']['core_site']['hadoop.security.authorization'] = true
         node.default['hadoop']['core_site']['hadoop.security.authentication'] = 'kerberos'
         node.default['krb5']['krb5_conf']['realms']['default_realm'] = 'example.com'
+        stub_command("kadmin -w password -q 'list_principals' | grep -v Auth | grep '^HTTP/fauxhai.local@EXAMPLE.COM'").and_return(true)
+        stub_command('test -e /etc/security/keytabs/HTTP.service.keytab').and_return(true)
       end.converge(described_recipe)
     end
 
-    it 'does nothing yet' do
-      expect(chef_run).to do_nothing
+    %w(hbase zookeeper).each do |user|
+      it "creates #{user} user" do
+        expect(chef_run).to create_user(user)
+      end
     end
   end
 end
