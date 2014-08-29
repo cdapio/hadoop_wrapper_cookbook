11a63f5104592494ff87f22749625a292a9b449a
Chris Gianelloni <chris@continuuity.com>
2014-08-28 20:02:50 -0700

    Move setting of attributes from recipe to attributes, and don't overwrite entire hash

--- attributes/krb5.rb
+++ attributes/krb5.rb
@@ -7,6 +7,17 @@ if node['hadoop'].key?('core_site') && node['hadoop']['core_site'].key?('hadoop.
   include_attribute 'krb5'
   include_attribute 'krb5_utils'
 
+  # Create service keytabs for all services, since we may be a client
+  default['krb5_utils']['krb5_service_keytabs']['HTTP'] = { 'owner' => 'hdfs', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['hdfs'] = { 'owner' => 'hdfs', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['hbase'] = { 'owner' => 'hbase', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['hive'] = { 'owner' => 'hive', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['jhs'] = { 'owner' => 'mapred', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['mapred'] = { 'owner' => 'mapred', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['yarn'] = { 'owner' => 'yarn', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_service_keytabs']['zookeeper'] = { 'owner' => 'zookeeper', 'group' => 'hadoop', 'mode' => '0640' }
+  default['krb5_utils']['krb5_user_keytabs']['yarn'] = { 'owner' => 'yarn', 'group' => 'hadoop', 'mode' => '0640' }
+
   # container-executor.cfg
   default['hadoop']['container_executor']['banned.users'] = 'hdfs,yarn,mapred,bin'
   default['hadoop']['container_executor']['min.user.id'] = 500
