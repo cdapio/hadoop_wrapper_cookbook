31b83225e12a5e8765e6766e9ec649ac6d3e4846
Chris Gianelloni <chris@cask.co>
2014-12-18 15:06:21 -0800

    Change to false on stub_command so bash block executes

--- spec/hadoop_hdfs_namenode_init_spec.rb
+++ spec/hadoop_hdfs_namenode_init_spec.rb
@@ -8,8 +8,8 @@ describe 'hadoop_wrapper::hadoop_hdfs_namenode_init' do
         node.automatic['memory']['total'] = '4099400kB'
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         # JDK 7
-        stub_command("echo '7a8d790e7bd9c2f82a83baddfae765797a4a56ea603c9150c87b7cdb7800194d  /var/chef/cache/jce7.zip' | sha256sum -c - >/dev/null").and_return(true)
-        stub_command("echo '7a8d790e7bd9c2f82a83baddfae765797a4a56ea603c9150c87b7cdb7800194d  /home/travis/.chef/cache/jce7.zip' | sha256sum -c - >/dev/null").and_return(true)
+        stub_command("echo '7a8d790e7bd9c2f82a83baddfae765797a4a56ea603c9150c87b7cdb7800194d  /var/chef/cache/jce7.zip' | sha256sum -c - >/dev/null").and_return(false)
+        stub_command("echo '7a8d790e7bd9c2f82a83baddfae765797a4a56ea603c9150c87b7cdb7800194d  /home/travis/.chef/cache/jce7.zip' | sha256sum -c - >/dev/null").and_return(false)
         stub_command('test -e /tmp/jce7/jce/US_export_policy.jar').and_return(false)
         stub_command('diff -q /tmp/jce7/jce/US_export_policy.jar /usr/lib/jvm/java/jre/lib/security/US_export_policy.jar').and_return(false)
       end.converge(described_recipe)
