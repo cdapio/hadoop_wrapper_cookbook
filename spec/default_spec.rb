e7d1025faf74c5b1c11c13687ea6ebb1b34ddf77
Chris Gianelloni <chris@cask.co>
2016-07-26 16:36:59 -0400

    COOK-78 Use java cookbook JCE support

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -7,15 +7,8 @@ describe 'hadoop_wrapper::default' do
         node.automatic['domain'] = 'example.com'
         node.automatic['memory']['total'] = '4099400kB'
         stub_command(/update-alternatives --display (.+) /).and_return(false)
-        stub_command(/jce(.+).zip' | sha256sum/).and_return(false)
-        stub_command(%r{test -e /tmp/jce(.+)/}).and_return(false)
-        stub_command(%r{diff -q /tmp/jce(.+)/}).and_return(false)
         stub_command(/test -L /).and_return(false)
       end.converge(described_recipe)
-    end
-
-    it 'includes _jce recipe' do
-      expect(chef_run).to include_recipe('hadoop_wrapper::_jce')
     end
 
     it 'includes kerberos_init recipe' do
