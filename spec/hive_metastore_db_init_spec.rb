42e5e87c31d61a9c8d93409f4f112ee1e1d57b92
Chris Gianelloni <chris@cask.co>
2016-10-19 21:10:10 -0400

    COOK-109 Use execute to load SQL files (#75)

--- spec/hive_metastore_db_init_spec.rb
+++ spec/hive_metastore_db_init_spec.rb
@@ -6,13 +6,14 @@ describe 'hadoop_wrapper::hive_metastore_db_init' do
       ChefSpec::SoloRunner.new(platform: 'centos', version: 6.6) do |node|
         node.automatic['domain'] = 'example.com'
         node.automatic['memory']['total'] = '4099400kB'
-        node.default['hive']['hive_site']['hive.metastore.uris'] = 'thrift://fauxhai.local:9083'
-        node.default['hive']['hive_site']['javax.jdo.option.ConnectionURL'] = 'jdbc:mysql://localhost:3306/hive'
+        node.override['hive']['hive_site']['hive.metastore.uris'] = 'thrift://fauxhai.local:9083'
+        node.override['hive']['hive_site']['javax.jdo.option.ConnectionURL'] = 'jdbc:mysql://localhost:3306/hive'
         stub_command(/update-alternatives --display (.+) /).and_return(false)
         stub_command(/jce(.+).zip' | sha256sum/).and_return(false)
         stub_command(%r{test -e /tmp/jce(.+)/}).and_return(false)
         stub_command(%r{diff -q /tmp/jce(.+)/}).and_return(false)
         stub_command(%r{/sys/kernel/mm/(.*)transparent_hugepage/defrag}).and_return(false)
+        stub_command(/test -L /).and_return(false)
       end.converge(described_recipe)
     end
   end
