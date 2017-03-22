32390e7f2f320f0042928cb1f3515d4daa2e23ee
Chris Gianelloni <chris@cask.co>
2017-03-22 23:27:08 +0000

    Version bump to 2.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'Apache 2.0'
 description      'Hadoop wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.0'
-
-%w(apt yum).each do |cb|
-  depends cb
-end
+version          '2.2.0'
 
 depends 'java', '~> 1.40'
 depends 'hadoop', '>= 2.0.0'
