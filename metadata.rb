2289c209c8a946758ef951b40dd8a9bcd8004739
Derek Wood <derek@cask.co>
2017-08-03 18:21:59 -0700

    require and test only chef > 12.5

--- metadata.rb
+++ metadata.rb
@@ -8,11 +8,11 @@ version          '3.0.0'
 
 depends 'java', '~> 1.40'
 depends 'hadoop', '>= 2.0.0'
-depends 'mysql', '~> 8.0'
+depends 'mysql', '~> 8.3'
 depends 'database', '~> 6.0'
 depends 'krb5', '>= 2.2.0'
 depends 'mysql2_chef_gem'
 
 source_url 'https://github.com/caskdata/hadoop_wrapper_cookbook' if respond_to?(:source_url)
 issues_url 'https://issues.cask.co/browse/COOK/component/10601' if respond_to?(:issues_url)
-chef_version '>= 11' if respond_to?(:chef_version)
+chef_version '>= 12.5' if respond_to?(:chef_version)
